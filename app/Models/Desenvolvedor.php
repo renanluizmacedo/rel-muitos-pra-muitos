131e0c1532fcd6edf96a761d7aecce94e98ee5f2
renanluizmacedo <renanluizmacedo@gmail.com>
2021-06-30 16:29:32 -0300

    utilizando belongstomany

--- app/Models/Desenvolvedor.php
+++ app/Models/Desenvolvedor.php
@@ -9,5 +9,8 @@ class Desenvolvedor extends Model
 {
     protected $table = 'desenvolvedores';
 
+    function projetos(){
+        return $this->belongsToMany("App\Models\Projeto","alocacoes")->withPivot('horas_semanais');
+    }
     use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Models\alocacao;
+use App\Models\Projeto;
+use App\Models\Desenvolvedor;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/desenvolvedor_projetos', function () {
+
+    $desenvolvedores = Desenvolvedor::with('projetos')->get();
+    
+    foreach($desenvolvedores as $d){
+        echo "<p> Nome do Desenvolvedor: ". $d->nome ."</p>";
+        echo "<p> Cargo ". $d->cargo ."</p>";
+ 
+        if(count($d->projetos) > 0){
+            echo "Projetos: <br>";
+            echo "<ul>";
+            foreach($d->projetos as $p){
+                echo "<li>";
+                echo "Nome:" .$p->nome . "|";
+                echo "Horas:" .$p->estimativa_horas;
+                echo "</li>";
+
+            }
+            echo "</ul>";
+        }
+        echo "<hr>";
+    }
+    
+    //return $desenvolvedores->toJson();
+});
+Route::get('/desenvolvedor_projetos_toJson', function () {
+
+    $desenvolvedores = Desenvolvedor::all();
+    
+    
+    return $desenvolvedores->toJson();
 });
